41c9cc8862065dc8df5a245b7e300972898e5eb8
jvsch <j.v.schneider@web.de>
2023-09-13 13:49:45 +0200

    new change

--- app.js
+++ app.js
@@ -5,20 +5,27 @@ const signupButton = document.getElementById("signup-button");
 const infoField = document.getElementById("info");
 const genderField = document.getElementById("select-menu");
 const profileImg = document.getElementById("profile-img");
+// const root = document.querySelector(":root");
+// const colorPicker = document.getElementById("color-picker");
 let username;
 let password;
 let account;
 
-function notValid() {
+function checkValidInput() {
   if (!(usernameField.value.length >= 3 && passwordField.value.length >= 3)) {
-    return 1;
+    infoField.innerText = `Try a longer password or username!`;
+    infoField.style.opacity = 1;
   } else if (
-    usernameField.value.indexOf(" ") !== -1 &&
-    passwordField.value.indexOf(" ") !== -1
+    usernameField.value.includes(" ") ||
+    passwordField.value.includes(" ")
   ) {
-    return 2;
+    infoField.innerText = `Spaces are not allowed!`;
+    infoField.style.opacity = 1;
+  } else if (signIn()) {
+    infoField.innerText = `"${usernameField.value}" is already taken!`;
+    infoField.style.opacity = 1;
   } else {
-    return 0;
+    return true;
   }
 }
 
@@ -40,8 +47,18 @@ function clearForm() {
   passwordField.value = "";
 }
 
-function createAccount() {
-  if (!signIn() && !notValid()) {
+submitButton.addEventListener("click", () => {
+  if (signIn()) {
+    window.location.href = "home/home.html";
+  } else {
+    infoField.innerText = `Wrong password or username!`;
+    infoField.style.opacity = 1;
+  }
+  clearForm();
+});
+
+signupButton.addEventListener("click", () => {
+  if (checkValidInput()) {
     account = {
       username: usernameField.value,
       password: passwordField.value,
@@ -49,28 +66,13 @@ function createAccount() {
     database.push(account);
     signupButton.setAttribute("disabled", "true");
     infoField.style.opacity = 0;
-  } else if (notValid() == 1) {
-    infoField.innerText = `Try a longer password or username!`;
-    infoField.style.opacity = 1;
-  } else if (notValid() == 2) {
-    infoField.innerText = `Spaces are not allowed!`;
-    infoField.style.opacity = 1;
-  } else {
-    infoField.innerText = `Username: ${usernameField.value} is already taken!`;
-    infoField.style.opacity = 1;
-  }
-  clearForm();
-}
-
-submitButton.addEventListener("click", () => {
-  if (signIn()) {
-    window.location.href = "https://youtube.com";
-  } else {
-    infoField.innerText = `Wrong password or username!`;
-    infoField.style.opacity = 1;
   }
   clearForm();
 });
+
+// colorPicker.addEventListener("change", () => {
+//   root.style.setProperty("--primary-color", colorPicker.value);
+// });
 
 genderField.addEventListener("change", () => {
   if (genderField.value == 1) {
@@ -79,8 +81,6 @@ genderField.addEventListener("change", () => {
     profileImg.setAttribute("src", "undraw_pic_profile_re_7g2h.svg");
   }
 });
-
-signupButton.addEventListener("click", createAccount);
 
 let database = [
   {
